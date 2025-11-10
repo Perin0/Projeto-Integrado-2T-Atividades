d2619af4953e37ecb5c50c6e50085011403b3f04
Perin0 <125603399+Perin0@users.noreply.github.com>
2025-11-10 23:29:34 +0000

    index.php complete

--- public/Plataforma de Cursos/index.php
+++ public/Plataforma de Cursos/index.php
@@ -1,5 +1,18 @@
 <?php
 include "lib/courses-functions.php";
+
+$currentArea = $_GET['area'] ?? null;
+$courses = getAllCourses($currentArea);
+
+$pageTitle = "Todos nossos cursos";
+if ($currentArea) {
+    $areaNames = [
+        'programming' => 'Cursos de Programação',
+        'design' => 'Cursos de Design',
+        'business' => 'Cursos de Negócios'
+    ];
+    $pageTitle = $areaNames[$currentArea] ?? "Cursos de " . ucfirst($currentArea);
+}
 ?>
 
 <!DOCTYPE html>
@@ -16,45 +29,40 @@ include "lib/courses-functions.php";
 
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <nav class="navbar navbar-expand-lg bg-primary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Navbar</a>
+            <a class="navbar-brand text-white" href="#">Unifio</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
+                        <a class="nav-link text-white" href="index.php">Todos os cursos</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
+                        <div class="btn-group">
+                            <a class="dropdown-toggle nav-link text-white" role="button" data-bs-toggle="dropdown">Cursos por área</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="index.php?area=programming">Programação</a></li>
+                                <li><a class="dropdown-item" href="index.php?area=design">Design</a></li>
+                                <li><a class="dropdown-item" href="index.php?area=business">Negócios</a></li>
+                            </ul>
+                        </div>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
 
-    <h1>Todos nossos cursos</h1>
+    <h1><?php echo htmlspecialchars($pageTitle); ?></h1>
 
     <div class="container py-4">
         <?php
-        $courses = getAllCourses();
         if ($courses === null) {
             echo '<div class="alert alert-danger">Não foi possível carregar os cursos.</div>';
+        } else if (empty($courses)){
+            echo '<div class="alert alert-info">Nenhum curso encontrado para esta área.</div>';
         } else {
             echo '<div class="row row-cols-1 row-cols-md-3 g-4">';
             foreach ($courses as $course) {
@@ -82,6 +90,7 @@ include "lib/courses-functions.php";
         }
         ?>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 
 </body>
 
--- public/Plataforma de Cursos/lib/courses-functions.php
+++ public/Plataforma de Cursos/lib/courses-functions.php
@@ -1,8 +1,31 @@
 <?php
 
-function getAllCourses(): ?array {
+function getAllCourses(?string $area = null): ?array {
     $path = __DIR__ . '/../courses.json';
+    
+    if (!file_exists($path)) {
+        return null; 
+    }
+
     $json = file_get_contents($path);
+    if ($json === false) {
+        return null;
+    }
+
     $data = json_decode($json, true);
-    return is_array($data) ? $data : null;
+    if ($data === null) {
+        return null;
+    }
+
+    if ($area === null) {
+        return $data;
+    }
+
+    
+    $filtered = array_filter($data, function ($course) use ($area) {
+        $courseArea = $course['area'] ?? $course['category'] ?? '';
+        return $courseArea === $area;
+    });
+
+    return array_values($filtered);
 }
